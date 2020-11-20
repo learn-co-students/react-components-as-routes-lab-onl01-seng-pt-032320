58caba07c5c54f235aa903a8c1a05f1b19337d26
joshpled <joshuapleduc@gmail.com>
2020-11-20 13:41:43 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -3,9 +3,10 @@ import { movies } from '../data';
 
 const Movies = () => {
   return (
-    <div>
-        {/*{code here}*/}
-    </div>
+   <div>
+     <h1>Movies Page</h1>
+      {movies.map((movie)=><div className="movie">Name: {movie.title}<br />Time:{movie.time}<br />Genres:<ul>{movie.genres.map((genre)=><li>{genre}</li>)}</ul></div>)}
+   </div>
   );
 };
 
