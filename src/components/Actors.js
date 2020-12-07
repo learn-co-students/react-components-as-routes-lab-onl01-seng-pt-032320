c8728403ae916b92951ab6f0788e7ce664f70210
jhar3ll <jharell.chambers@selu.edu>
2020-12-06 18:57:25 -0600

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,9 +4,21 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page </h1>
+        {actors.map((actor, i) => 
+        <div
+        key={i}
+        className="actor"> 
+          Actor: {actor.name}
+          <ul
+          className="actor-movies">
+           Movies: {actor.movies.map((movie, i) => <li key={i}> {movie} </li>)}
+          </ul>
+          <br></br>
+        </div>
+        )}
     </div>
-  );
-};
+  )
+}
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,22 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+  
   return (
     <div>
-        {/*{code here}*/}
+        <h1> Movies Page </h1>
+        {movies.map((movie, i) => 
+        <div
+        key={i}
+        className="movie"> 
+          Movie: {movie.title} ({movie.time} min)
+          <ul
+          className="movie-genres">
+           Genres: {movie.genres.map((genre, i) => <li key={i}> {genre} </li>)}
+          </ul>
+          <br></br>
+        </div>
+        )}
     </div>
   );
 };
