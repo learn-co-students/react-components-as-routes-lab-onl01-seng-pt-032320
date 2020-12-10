9bf9456e260e92fa15619237420194b068b52db7
AcnologiatheEnd <18.chung.winston@ctkprep.org>
2020-12-10 00:12:16 -0500

    Done

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import { actors } from '../data';
-
+const actorInfo = (actor) => {
+  console.log(actor);
+  return(<div className="actor">
+    <h1>{actor.name}</h1>
+    <ul>
+      {actor.movies.map((movie, index) => (
+      <li key={index}>{movie}</li>
+      ))}
+    </ul>
+  </div>
+  )
+};
+const string = "Actors list"
 const Actors = () => {
+
   return (
+  
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map(actorInfo)}
     </div>
   );
+  
 };
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import { movies } from '../data';
 
+const movieslist = (movie) => {
+  return(
+  <div className="movies">
+    <h2>{movie.title}</h2>
+    <h2>{movie.time}</h2>
+  <ul>
+    {movie.genres.map((genre) => (
+      <li>{genre}</li>
+    ))}
+  </ul>
+  </div>
+  )
+};
+
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+        {movies.map(movieslist)}
     </div>
   );
 };
