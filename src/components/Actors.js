583c45104e7efac09b6e28bfbfb7bcc9c3fda81a
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2020-12-17 00:25:41 -0600

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,9 +4,17 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+        <h1>Actors Page</h1>
+        { actors.map((actor, index) => 
+          <div className='actor' key={index}>
+            { actor.name }
+            <ul>
+              {actor.movies.map(movie => <li>{movie}</li>)}
+            </ul>
+          </div>
+        )}
     </div>
-  );
+  )
 };
 
 export default Actors;
